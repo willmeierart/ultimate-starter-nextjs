7007765ad18e5a70c76998ec82922372685f6b6b
WILL MEIER <willmeierart@gmail.com>
2017-11-27 15:37:13 -0700

    update menu

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -1,6 +1,13 @@
-export default () => (
-  <div>
-    <div></div>
-    <style jsx>{``}</style>
-  </div>
-)
+export default () => {
+  // const renderList = (set, element) => {
+  //   return set.map((each, i) => (<element key={i}>{ each }</element>))
+  // }
+  return (
+    <div>
+      <div>
+        {/* { renderList() } */}
+      </div>
+      <style jsx>{``}</style>
+    </div>
+  )
+}
