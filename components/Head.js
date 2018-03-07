b83cab39dc6d00e3392127b3b6edfc22c29ce2f7
WILL MEIER <willmeierart@gmail.com>
2018-03-07 10:06:20 -0700

    fix inital initial bug

--- components/Head.js
+++ components/Head.js
@@ -11,7 +11,7 @@ const CustomHead = (props = initialProps) => {
   return <Head>
     <title key='title'>{title}</title>
     <meta key='charset' charSet='utf-8' />
-    <meta key='viewport' name='viewport' content={`inital-scale=${initialScale || initialProps.initialScale}, width=device-width, shrink-to-fit=no`} />
+    <meta key='viewport' name='viewport' content={`initial-scale=${initialScale || initialProps.initialScale}, width=device-width, shrink-to-fit=no`} />
     <meta key='meta-title' name='title' content='Agency Zero' />
     <link rel='shortcut icon' href='/favicon.ico' />
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
