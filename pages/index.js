1a8be7f7296b29e2ff3c1cb78dd656886df64c59
WILL MEIER <willmeierart@gmail.com>
2018-03-08 12:49:05 -0700

    update for static getinitialprops in index page

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,17 @@
 import React, { Component } from 'react'
 import AppProvider from '../lib/redux/AppProvider'
 import { AzLogo01 } from '../components/assets/ZeroLogos'
+import fetch from 'isomorphic-fetch'
 
 // include boilerplate for global loader dependent on graphql req's:
 export default class HomePage extends Component {
+  static async getInitialProps () {
+    const API_URL = ''
+    const res = await fetch(API_URL)
+    const json = await res.json()
+    const { thing } = json
+    return thing
+  }
   render () {
     return (
       <AppProvider title='Home'>
