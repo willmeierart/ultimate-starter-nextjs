a8989cd3d47edc7b3e4544332cf03f094a1fda36
Will Meier <willmeierart@gmail.com>
2018-10-09 10:34:21 -0600

    modify docker-compose

--- _scripts/pre/installDocker.sh
+++ _scripts/pre/installDocker.sh
@@ -5,3 +5,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+sudo apt install docker-compose -y
