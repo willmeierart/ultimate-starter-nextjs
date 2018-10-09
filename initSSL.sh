651bef42ea05794b97004cde7fc401802f982702
Will Meier <willmeierart@gmail.com>
2018-10-09 11:55:59 -0600

    update init scripts

---
+++ initSSL.sh
@@ -0,0 +1,24 @@
+
+# ./_scripts/pre/installJenkins.sh
+if [ $1 ]
+  then
+    echo "building docker image as $1"
+    export PROJECT_NAME=$1
+    /bin/sh ./_scripts/pre/installDocker.sh
+    /bin/sh ./_scripts/pre/letsencrypt/staging.sh \
+      && /bin/sh ./_scripts/pre/letsencrypt/production.sh \
+      && /bin/sh ./_scripts/pre/letsencrypt/cron.sh \
+    docker image build -t $1 . \
+      && docker container run -it --name app -p 80:3000 $1
+else
+  echo "what should this docker image be called?"
+  read IMAGE_NAME
+  echo "building docker image as $IMAGE_NAME"
+  export PROJECT_NAME=$IMAGE_NAME
+  /bin/sh ./_scripts/pre/installDocker.sh
+  /bin/sh ./_scripts/pre/letsencrypt/staging.sh \
+      && /bin/sh ./_scripts/pre/letsencrypt/production.sh \
+      && /bin/sh ./_scripts/pre/letsencrypt/cron.sh \
+  docker image build -t agencyzero/$IMAGE_NAME . \
+    && docker container run -it --name app -p 80:3000 $IMAGE_NAME
+fi
