20ce25052f3dead4fc27d389c4d5077bb5c91788
Will Meier <willmeierart@gmail.com>
2018-10-09 10:47:43 -0600

    modify docker-compose

--- init.sh
+++ init.sh
@@ -3,12 +3,14 @@
 if [ $1 ]
   then
     echo "building docker image as $1"
+    export PROJECT_NAME=$1
     docker image build -t $1 . \
     && docker container run -it --name app -p 80:3000 $1
 else
   echo "what should this docker image be called?"
   read IMAGE_NAME
   echo "building docker image as $IMAGE_NAME"
+  export PROJECT_NAME=$IMAGE_NAME
   docker image build -t agencyzero/$IMAGE_NAME . \
   && docker container run -it --name app -p 80:3000 $IMAGE_NAME
 fi
