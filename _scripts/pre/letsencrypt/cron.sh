9a7da9414935613f255d6dbc46f43291cb6acda8
Will Meier <willmeierart@gmail.com>
2018-10-09 11:53:29 -0600

    add initial ssl scripts and compose files

---
+++ _scripts/pre/letsencrypt/cron.sh
@@ -0,0 +1,5 @@
+sudo crontab -l > ssl \
+  && echo 0 23 * * * docker run --rm -it --name certbot -v "/docker-volumes/etc/letsencrypt:/etc/letsencrypt" -v "/docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt" -v "/docker-volumes/data/letsencrypt:/data/letsencrypt" -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" certbot/certbot renew --webroot -w /data/letsencrypt --quiet && docker kill --signal=HUP production-nginx-container >> mycron \
+  && crontab ssl \
+  && rm ssl
+  
---
+++ _scripts/pre/letsencrypt/staging.sh
@@ -0,0 +1,3 @@
+sudo mkdir -p /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site
+sudo cp ../../../_deployment/proxy/letsencrypt/staging/docker-compose.yml /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site
+sudo cp ../../../_deployment/proxy/letsencrypt/staging/nginx.conf /docker/letsencrypt-docker-nginx/src/letsencrypt/nginx.conf
