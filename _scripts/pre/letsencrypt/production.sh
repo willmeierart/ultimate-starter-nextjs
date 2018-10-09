92cf79e02d91ed25f20d89ac6af4fe8dff45a02e
Will Meier <willmeierart@gmail.com>
2018-10-09 11:58:37 -0600

    update init scripts

--- _scripts/pre/letsencrypt/production.sh
+++ _scripts/pre/letsencrypt/production.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /docker/letsencrypt-docker-nginx/src/production/production-site
 sudo mkdir -p /docker/letsencrypt-docker-nginx/src/production/dh-param
-sudo cp ../../../_deployment/proxy/letsencrypt/production/docker-compose.yml /docker/letsencrypt-docker-nginx/src/production/production-site
-sudo cp ../../../_deployment/proxy/letsencrypt/production/nginx.conf /docker/letsencrypt-docker-nginx/src/letsencrypt/nginx.conf
+sudo cp ./_deployment/proxy/letsencrypt/production/docker-compose.yml /docker/letsencrypt-docker-nginx/src/production/production-site
+sudo cp ./_deployment/proxy/letsencrypt/production/nginx.conf /docker/letsencrypt-docker-nginx/src/letsencrypt/nginx.conf
 sudo openssl dhparam -out /docker/letsencrypt-docker-nginx/src/production/dh-param/dhparam-2048.pem 2048
