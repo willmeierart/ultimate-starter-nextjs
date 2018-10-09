ea6a9244461286ff73a66ffbf03fa5ad963663d8
Will Meier <willmeierart@gmail.com>
2018-10-09 13:41:49 -0600

    update crontab ssl

--- _scripts/pre/letsencrypt/cron.sh
+++ _scripts/pre/letsencrypt/cron.sh
@@ -1,5 +1,6 @@
+touch /var/spool/cron/crontabs/ssl
 sudo crontab -l > ssl \
   && echo 0 23 * * * docker run --rm -it --name certbot -v "/docker-volumes/etc/letsencrypt:/etc/letsencrypt" -v "/docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt" -v "/docker-volumes/data/letsencrypt:/data/letsencrypt" -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" certbot/certbot renew --webroot -w /data/letsencrypt --quiet && docker kill --signal=HUP production-nginx-container >> mycron \
-  && crontab ssl \
+  && sudo crontab ssl \
   && rm ssl
   
