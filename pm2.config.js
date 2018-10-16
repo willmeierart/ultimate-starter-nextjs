f701ea79fb3ed2469546742e8a1722ff2d0483d5
Will Meier <willmeierart@gmail.com>
2018-10-16 14:42:29 -0600

    update dockerfile, add pm2 config file

---
+++ pm2.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps : [{
+    name: 'client',
+    script: 'npm --no-automation -- run start',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    autorestart: true,
+    watch: false,
+    // max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   }
+  // }
+};
