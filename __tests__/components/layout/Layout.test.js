11098ad51bb148288968b9ee15041567fc96038f
WILL MEIER <willmeierart@gmail.com>
2018-11-08 16:27:36 -0700

    fix tests

--- __tests__/components/layout/Layout.test.js
+++ __tests__/components/layout/Layout.test.js
@@ -4,43 +4,41 @@ import { shallow } from 'enzyme'
 import configureStore from 'redux-mock-store'
 import { connect } from 'react-redux'
 import { storeMode } from '../../../lib/globals'
-import Layout from '../../../components/_layout'
+import Layout from '../../../components/_layout/Layout'
 
 const initialState = {
-  env: {
-    isMobile: false,
-    mobileSideways: false,
-    isIE: false,
-    browser: 'unknown',
-    vpDims: {
-      width: 0,
-      height: 0
-    },
-    screenLocked: false,
-    isFreshLoad: true
-  },
-  ui: {
-    menuOpen: false
-  }
+	env: {
+		isMobile: false,
+		mobileSideways: false,
+		isIE: false,
+		browser: 'unknown',
+		vpDims: {
+			width: 0,
+			height: 0
+		},
+		screenLocked: false,
+		isFreshLoad: true
+	},
+	ui: {
+		menuOpen: false
+	}
 }
 const mockStore = configureStore()
 
 describe('Layout', () => {
-  let store
-  const props = {
-    onCheckIfMobile: jest.fn(),
-    onGetVPDims: jest.fn(),
-    isMobile: false
-  }
+	let store
+	const props = {
+		onCheckIfMobile: jest.fn(),
+		onGetVPDims: jest.fn(),
+		isMobile: false
+	}
 
-  beforeEach(() => {
-    store = mockStore(initialState)
-  })
+	beforeEach(() => {
+		store = mockStore(initialState)
+	})
 
-  it('renders correctly', () => {
-    const layout = shallow(<Layout store={store} {...props} />)
-    expect(layout).toMatchSnapshot()
-  })
-
-  
+	it('renders correctly', () => {
+		const layout = shallow(<Layout store={store} {...props} />)
+		expect(layout).toMatchSnapshot()
+	})
 })
--- __tests__/pages/_app.test.js
+++ __tests__/pages/_app.test.js
@@ -7,81 +7,73 @@ import { storeMode } from '../../lib/globals'
 import configureStore from 'redux-mock-store'
 import MyApp, { StateMgmtSwitch } from '../../pages/_app'
 import HomePage from '../../pages/index'
-import Layout from '../../components/_layout'
+import Layout from '../../components/_layout/Layout'
 import { Provider } from 'react-redux'
 import GlobalContextProvider from '../../components/__contextProviders/GlobalContextProvider'
 
-
 const initialState = {
-  env: {
-    isMobile: false,
-    mobileSideways: false,
-    isIE: false,
-    browser: 'unknown',
-    vpDims: {
-      width: 0,
-      height: 0
-    },
-    screenLocked: false,
-    isFreshLoad: true
-  },
-  ui: {
-    menuOpen: false
-  }
+	env: {
+		isMobile: false,
+		mobileSideways: false,
+		isIE: false,
+		browser: 'unknown',
+		vpDims: {
+			width: 0,
+			height: 0
+		},
+		screenLocked: false,
+		isFreshLoad: true
+	},
+	ui: {
+		menuOpen: false
+	}
 }
 const mockStore = configureStore()
 
 describe('MyApp', () => {
-  let store
-  let myApp = shallow(<MyApp {...props} />)
-  let props = {}
+	let store
+	let myApp = shallow(<MyApp {...props} />)
+	let props = {}
 
-  beforeEach(() => {
-    props.store = store
-    store = mockStore(initialState)
-  })
+	beforeEach(() => {
+		props.store = store
+		store = mockStore(initialState)
+	})
 
-  it('renders correctly', () => {
-    expect(myApp).toMatchSnapshot()
-  })
+	it('renders correctly', () => {
+		expect(myApp).toMatchSnapshot()
+	})
 
-  // it('should have correct props', () => {
-  //   const mountedApp = mount(<MyApp />)
-  //   expect(mountedApp.props()).toExist()
-  // })
+	// it('should have correct props', () => {
+	//   const mountedApp = mount(<MyApp />)
+	//   expect(mountedApp.props()).toExist()
+	// })
 
-  // it('should call `getInitialProps`', async () => {
-  //   const pageProps = {
-  //     ctx: {},
-  //     Component: HomePage,
-  //     router: {}
-  //   }
+	// it('should call `getInitialProps`', async () => {
+	//   const pageProps = {
+	//     ctx: {},
+	//     Component: HomePage,
+	//     router: {}
+	//   }
 
-  //   const initialProps = await MyApp.getInitialProps(pageProps)
+	//   const initialProps = await MyApp.getInitialProps(pageProps)
 
-  //   const mountedApp = mount(
-  //     <MyApp {...props} {...pageProps}>
-  //       <HomePage />
-  //     </MyApp>
-  //   )
-  //   expect(mountedApp.getInitialProps).toHaveBeenCalled()
-  // })
+	//   const mountedApp = mount(
+	//     <MyApp {...props} {...pageProps}>
+	//       <HomePage />
+	//     </MyApp>
+	//   )
+	//   expect(mountedApp.getInitialProps).toHaveBeenCalled()
+	// })
 
-  describe('StateManagementSwitch', () => {
-    it('renders the correct `store` component', () => {
-      const correctChild = storeMode === 'REDUX'
-        ? shallow(
-            <StateMgmtSwitch store={store}>
-              { <Layout /> }
-            </StateMgmtSwitch>
-          )
-        : shallow(
-            <GlobalContextProvider store={store}>
-              { <Layout /> }
-            </GlobalContextProvider>
-          )
+	describe('StateManagementSwitch', () => {
+		it('renders the correct `store` component', () => {
+			const correctChild =
+				storeMode === 'REDUX'
+					? shallow(<StateMgmtSwitch store={store}>{<Layout />}</StateMgmtSwitch>)
+					: shallow(<GlobalContextProvider store={store}>{<Layout />}</GlobalContextProvider>)
 
-      expect(correctChild.exists()).toBe(true)
-    })
-  })
+			expect(correctChild.exists()).toBe(true)
+		})
+	})
 })
